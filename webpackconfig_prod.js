99cf960eca2f50f26153af1e4942ae1a2da467fb
mimiring <island_2@naver.com>
2020-04-15 18:27:01 +0900

    모드 설정(모드 별 설정 파일 작성)

--- webpackconfig_prod.js
+++ webpackconfig_prod.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: "production",
+    mode: "development",
     entry: "./source/index.js",
     output: {
         path: path.resolve(__dirname, "public"),
