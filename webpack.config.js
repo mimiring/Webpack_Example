00f050a0532e2255b8f2e1812815c1e671393455
mimiring <island_2@naver.com>
2020-04-15 19:02:25 +0900

    loader 설치 및 설정

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: "./source/index.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.css$/, // 확장자가 css인 파일을 처리하는 방법을 알려주고 싶을 때
+                use: [
+                    'style-loader', // 가져온css 코드를 웹페이지 안에 주입
+                    'css-loader' // css파일을 읽어옴
+                ]
+            }
+        ]
+    }
+}
