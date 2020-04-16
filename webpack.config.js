b8c01c93c8a30bfe37acc776788434fe23665879
mimiring <island_2@naver.com>
2020-04-16 19:16:32 +0900

    플러그인 설치 및 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -20,5 +21,17 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './source/index.html',
+            filename: './index.html',
+            chunks: ['index']
+        }),
+        new HtmlWebpackPlugin({
+            template: './source/about.html',
+            filename: './about.html',
+            chunks: ['about']
+        })
+    ]
 }
